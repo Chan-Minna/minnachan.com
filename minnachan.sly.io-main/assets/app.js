9227a6225eb0ffa8fd53191d7042768058439c41
Minna Chan <71239539+Chan-Minna@users.noreply.github.com>
2022-11-19 16:47:45 -0500

    Delete minnachan.sly.io-main directory

--- minnachan.sly.io-main/assets/app.js
+++
@@ -1,9 +0,0 @@
-function sb_open() {
-    document.getElementsByTagName("nav")[0].style.display = "block";
-    document.getElementById("overlay").style.display = "block";
-}
-
-function sb_close() {
-    document.getElementsByTagName("nav")[0].style.display = "none";
-    document.getElementById("overlay").style.display = "none";
-}
