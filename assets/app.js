f3c5b043130dfea872db1bdc7730632a11bc52c4
Minna Chan <71239539+Chan-Minna@users.noreply.github.com>
2024-08-25 14:11:11 -0400

    Add files via upload

--- assets/app.js
+++ assets/app.js
@@ -7,3 +7,19 @@ function sb_close() {
     document.getElementsByTagName("nav")[0].style.display = "none";
     document.getElementById("overlay").style.display = "none";
 }
+
+function show_modal(event) {
+    event.currentTarget.nextElementSibling.style.visibility = "visible";
+}
+var asdf;
+function hide_modal(event) {
+    if (event.target.localName != "model-viewer")
+        event.currentTarget.style.visibility = "hidden";
+}
+
+for (let element of document.getElementById("card-container").getElementsByClassName("card")){
+    element.addEventListener("click", show_modal); 
+}
+for (let element of document.getElementById("card-container").getElementsByClassName("modal")){
+    element.addEventListener("click", hide_modal); 
+}
